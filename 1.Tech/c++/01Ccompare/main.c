392869dbba7d959034f849f198de11fa0b22e14f
tzj <290717997@qq.com>
2019-02-26 00:43:32 +0800

    c++实战

---
+++ 1.Tech/c++/01Ccompare/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char *p = malloc(100);
+    return 0;
+}
---
+++ 1.Tech/c++/01typeEnhance/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    char *p = malloc(100);
+
+    return 0;
+}
